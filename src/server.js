a6745cd24aafbf494469e5b5a79de7e705b144c4
Nozen <enzo.reboul345@gmail.com>
2025-04-02 15:47:23 +0200

    fix bug

--- src/server.js
+++ src/server.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const compression = require('compression');
-const pool = require('./configs/database')
-
-//insert import routes
-const exampleRoutes = require('./routes/exampleRoutes');
 
 dotenv.config({ path: '.env.local' });
-
-connection.connect(error => {
-    if(error) throw error;
-    console.log("connecté a la base de donnée")
-})
 
 const app = express();
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
-
-
-// insert routes here
-app.use('/entities', exampleRoutes);
 
 
 const PORT = process.env.PORT || 3000;
